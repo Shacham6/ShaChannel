0e81a4fa84cdda4d5d58c41e89e5205aa7ea7ed1
shach <shacham6@gmail.com>
2017-10-08 19:52:26 +0200

    Defined the interface of ChannelReader

--- src/ChannelReader.kt
+++ src/ChannelReader.kt
@@ -1,3 +1,11 @@
-interface ChannelReader<out T> {
+import java.util.concurrent.TimeUnit
+
+interface ChannelReader<T> {
     fun receive(): T
+    fun receiveOrDefaultWithinTimeout(waitDuration: Duration, defaultValue: T?): T?
+
+    @Throws(ChannelReaderException::class)
+    fun receive(waitDuration: Duration): T
+
+    fun clone(): ChannelReader<T>
 }
