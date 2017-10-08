8c41d804c0db994d776bb297957edb0712b5b46c
Shacham <shacham6@gmail.com>
2017-10-08 20:25:57 +0200

    Added a basic test that will be expanded upon. Holy shit, having built-in JUnit is fucking awesome dude

--- test/org/shach/channels/ChannelReaderImplTest.kt
+++ test/org/shach/channels/ChannelReaderImplTest.kt
@@ -1,5 +1,15 @@
 package org.shach.channels
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import java.util.concurrent.LinkedTransferQueue
 
-internal class ChannelReaderImplTest
+internal class ChannelReaderImplTest {
+    @Test
+    fun testBasicReading() {
+        val queue = LinkedTransferQueue<String>()
+        val reader = ChannelReaderImpl<String>(queue)
+        queue.add("Praise The Sun!!")
+        assertEquals("Praise The Sun!!", reader.receive())
+    }
+}
