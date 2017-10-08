dbe4be358d3310c1be8053064a0c45fccca0ba95
Shacham <shacham6@gmail.com>
2017-10-08 20:22:27 +0200

    Created a test file and changed the access modifier on the ChannelWriter implementation

--- src/org/shach/channels/ChannelWriterImpl.kt
+++ src/org/shach/channels/ChannelWriterImpl.kt
@@ -2,7 +2,7 @@ package org.shach.channels
 
 import java.util.concurrent.TransferQueue
 
-class ChannelWriterImpl<in T>(private val dataQueue: TransferQueue<T>) : ChannelWriter<T> {
+internal class ChannelWriterImpl<in T>(private val dataQueue: TransferQueue<T>) : ChannelWriter<T> {
     override fun send(vararg data: T) {
         for (item in data) {
             dataQueue.transfer(item)
---
+++ test/org/shach/channels/ChannelReaderImplTest.kt
@@ -0,0 +1,5 @@
+package org.shach.channels
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class ChannelReaderImplTest
